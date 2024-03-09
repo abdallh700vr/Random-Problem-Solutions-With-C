f4f971b9a67043caa9923695cf1286a61ff73d59
ABDULLAH HAN (FOXY) <abdallhameed2005@gmail.com>
2024-03-09 19:01:42 +0300

    Update deitel.7.28.c

--- deitel.7.28.c
+++ deitel.7.28.c
@@ -26,19 +26,9 @@ int choice=0;
 printf(" please enter what opertor you want to do : \n");
 scanf("%d",&choice);
 
-if(choice==0)
-    (*notIslemleri[choice])(ogrenciNotlari);
-else if(choice==1)
-    (*notIslemleri[choice])(ogrenciNotlari);
-else if(choice==2)
-    (*notIslemleri[choice])(ogrenciNotlari);
-else if(choice==3)
-    (*notIslemleri[choice])(ogrenciNotlari);
-else
-{
-    printf("error \n \n");
-}
 
+    (*notIslemleri[choice])(ogrenciNotlari);
+   
     return 0;
 }
 void diziyiYazdir( const int ogrenciNotlari[][SINAVLAR])
